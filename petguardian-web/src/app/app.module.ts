917af975850d67b5be63ae59ec262b7cf7d5f4ce
Pau Llobet <pllobet@mychefpro.com>
2023-10-05 17:55:44 +0200

    Feat: navbar added

--- petguardian-web/src/app/app.module.ts
+++ petguardian-web/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
