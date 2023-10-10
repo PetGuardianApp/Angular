d05fd2e9c1ede1fd2bacb3cf4f8ed401c3226687
David Castro Sanchez <david.castro.sachez@gmail.com>
2023-10-10 18:11:12 +0200

    StorageService created and loggedin as observable

--- petguardian-web/src/app/side-nav/side-nav.component.ts
+++ petguardian-web/src/app/side-nav/side-nav.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { StorageService } from '../services/storage.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent {
+export class SideNavComponent  {
+
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private storageService: StorageService) {
+    this.isLoggedIn$ = this.storageService.isLoggedIn;
+   }
+
+  
 
 }
